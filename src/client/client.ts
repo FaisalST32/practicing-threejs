443322e0f6c832d527e463a0a18bf01e35f7827a
Faisal Rashid <faisalrashid32@outlook.com>
2021-08-28 11:22:13 +0530

    implemented pcd

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,75 +1,117 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import Stats from 'three/examples/jsm/libs/stats.module';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module';
-import { AxesHelper } from 'three';
+
+import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
-scene.add(new AxesHelper(5));
+scene.add(new THREE.AxesHelper(400000000000000000000));
 
+const light = new THREE.PointLight();
+light.position.set(2.5, 7.5, 15);
+scene.add(light);
 const camera = new THREE.PerspectiveCamera(
-	75,
+	15,
 	window.innerWidth / window.innerHeight,
 	0.1,
-	1000
+	10000000000
 );
-camera.position.z = 2;
+// camera.position.x = 496436.9375 * 1;
+// camera.position.y = 5422159.5 * 1;
+camera.position.z = 300;
 
-// const camera2 = new THREE.OrthographicCamera(-2, 2, 2, -2);
-// camera2.position.z = 2;
+scene.add(camera);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// const canvas1 = document.getElementById('c1') as HTMLCanvasElement;
-// const canvas2 = document.getElementById('c2') as HTMLCanvasElement;
-const renderer1 = new THREE.WebGLRenderer();
-// const renderer2 = new THREE.WebGLRenderer({ canvas: canvas2 });
-renderer1.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer1.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
-const controls = new OrbitControls(camera, renderer1.domElement);
-controls.addEventListener('change', render);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-	color: new THREE.Color('rgb(255, 255, 255)'),
+const material = new THREE.MeshNormalMaterial({
 	wireframe: false,
-	opacity: 0.4,
 });
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-scene.background = new THREE.Color(0x000000);
-
 const gui = new GUI();
-const cubeFolder = gui.addFolder('Cube');
-cubeFolder.open();
-const cubeRotationFolder = cubeFolder.addFolder('Rotation');
-cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
-cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
-cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
-cubeRotationFolder.open();
-
-const cubeScaleFolder = cubeFolder.addFolder('Scale');
-cubeScaleFolder.add(cube.scale, 'x', 0, 5);
-cubeScaleFolder.add(cube.scale, 'y', 0, Math.PI * 2);
-cubeScaleFolder.add(cube.scale, 'z', 0, Math.PI * 2);
-cubeScaleFolder.open();
-
-const cubePositionFolder = cubeFolder.addFolder('Position');
-cubePositionFolder.add(cube.position, 'x', 0, 5);
-cubePositionFolder.add(cube.position, 'y', 0, 5);
-cubePositionFolder.add(cube.position, 'z', 0, 5);
-cubePositionFolder.open();
-cubeFolder.add(cube, 'visible');
-
 const cameraFolder = gui.addFolder('Camera');
-cameraFolder.add(camera.position, 'z', 0, 10);
+cameraFolder.add(camera.position, 'x', 495336.9375 * 1, 496436.9375 * 1);
+cameraFolder.add(camera.position, 'y', 5421059.5, 5425159.5);
+cameraFolder.add(
+	camera.position,
+	'z',
+	5422159.489990234375 * 0.6,
+	5422159.489990234375 * 1.4
+);
 cameraFolder.open();
+
+// const objLoader = new OBJLoader();
+// objLoader.load(
+// 	'models/monkey.obj',
+// 	(object) => {
+// 		// (object.children[0] as THREE.Mesh).material = material
+// 		object.traverse(function (child) {
+// 			if ((child as THREE.Mesh).isMesh) {
+// 				(child as THREE.Mesh).material = material;
+// 			}
+// 		});
+// 		scene.add(object);
+// 	},
+// 	(xhr) => {
+// 		console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+// 	},
+// 	(error) => {
+// 		console.log(error);
+// 	}
+// );
+
+// objLoader.load(
+// 	'models/cube.obj',
+// 	(object) => {
+// 		object.position.x = 2;
+// 		scene.add(object);
+// 	},
+// 	(xhr) => {
+// 		console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+// 	},
+// 	(error) => {
+// 		console.log(error);
+// 	}
+// );
+
+// var loader = new PCDLoader();
+// loader.load(
+// 	'models/map.pcd',
+// 	function (points) {
+// 		console.log(points);
+// 		(points.material as THREE.PointsMaterial).size = 1;
+// 		// (points.material as THREE.PointsMaterial).color = new THREE.Color(0xffffff);
+// 		// points.scale.multiplyScalar(0.000001);
+// 		// console.log(points);
+// 		// points.frustumCulled = false;
+// 		points.position.x = -512333;
+// 		points.position.y = -5403400;
+// 		points.position.z = -316;
+// 		points.frustumCulled = false;
+// 		scene.add(points);
+// 	},
+// 	(xhr) => {
+// 		console.log(`Loading ${Math.ceil((xhr.loaded / xhr.total) * 100)}%`);
+// 	}
+// );
+
+var loader = new FBXLoader();
+loader.load('models/vanguard_t_choonyung.fbx', (model) => {
+	model.scale.set(0.01, 0.01, 0.01);
+	scene.add(model);
+});
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	renderer1.setSize(window.innerWidth, window.innerHeight);
+	renderer.setSize(window.innerWidth, window.innerHeight);
 	render();
 }
 
@@ -79,15 +121,15 @@ document.body.appendChild(stats.dom);
 function animate() {
 	requestAnimationFrame(animate);
 
-	// cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.01;
-	stats.update();
+	controls.update();
+
 	render();
+
+	stats.update();
 }
 
 function render() {
-	renderer1.render(scene, camera);
-	// renderer2.render(scene, camera2);
+	renderer.render(scene, camera);
 }
-render();
+
 animate();
